626fc3f12fdc291eedb120f9e937257e82161bc8
Nithin Manari Venkata Satya <nithin.mannari@gmail.com>
2016-10-25 20:50:16 +0000

    Add Email with SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = {:host => 'https://saas-project-app-nithinmannari.c9users.io'}
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -26,7 +29,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
